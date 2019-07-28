87847b561e1fcae950c449bec42606a26b1ac1dc
Prakul Kumar (Digital) <PR354342@wipro.com>
2019-07-28 14:31:43 +0530

    grid name change

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+import { Navbar } from 'react-bootstrap';
+
+function HeaderNavbar() {
+    return (
+        <Navbar bg="dark" className="navbar__mainContainer">
+            ROOM BOOKING APP
+        </Navbar>
+    )
+}
+
+export default HeaderNavbar;
