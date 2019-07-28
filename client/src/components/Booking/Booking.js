ef7bfc4f638f34274a363fa7270194daae837fc5
Randeep Kaur (Digital) <RA354347@wipro.com>
2019-07-28 13:52:05 +0530

    state for booking added

--- client/src/components/Booking/Booking.js
+++ client/src/components/Booking/Booking.js
@@ -2,6 +2,37 @@ import React, { Component } from 'react';
 
 class Booking extends Component {
 
+    state = {
+        validated: false,
+        hotelBookingForm: {
+            step: 1,
+            firstName: '',
+            lastName: '',
+            address: '',
+            checkIn: '',
+            checkOut: '',
+            adults: '',
+            children: 0,
+            contactNumber: '',
+            rooms: [],
+            amount: '',
+            advance: ''
+        },
+        cancel: false,
+        checkedIn: false,
+        checkedOut: false,
+        availableRooms: [],
+        formIsValid: true,
+        isEdit: false,
+        bookingId: null,
+        personId: null,
+        disable: false,
+        misc: '',
+        total: '',
+        balance: '',
+        status: ''
+    }
+
     render() {
         return <React.Fragment></React.Fragment>
     }
