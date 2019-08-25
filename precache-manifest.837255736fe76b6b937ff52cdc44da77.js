7c5b2bf9c7a1e7000581d66ea0e5d0f3f2a4bca4
Prakul Kumar (Digital) <PR354342@wipro.com>
2019-08-25 13:01:22 +0530

    Updates

---
+++ precache-manifest.837255736fe76b6b937ff52cdc44da77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf300d2719fe24e1e52a6a62f389c66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00e5f2ba43e384eb2256",
+    "url": "/static/css/2.9045a562.chunk.css"
+  },
+  {
+    "revision": "211e81f233ff55249030",
+    "url": "/static/css/main.09d00957.chunk.css"
+  },
+  {
+    "revision": "00e5f2ba43e384eb2256",
+    "url": "/static/js/2.af119ace.chunk.js"
+  },
+  {
+    "revision": "211e81f233ff55249030",
+    "url": "/static/js/main.081251dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
